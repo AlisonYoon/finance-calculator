1cf794d090f408034ff734fa2aa9826c1e116eb9
AlisonYoon <politescarlett1@gmail.com>
2019-09-02 21:37:18 +0100

    display admin fee

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function updateSlider(slideAmount) {
-  var sliderDiv = document.getElementById("sliderAmount");
-  sliderDiv.innerHTML = slideAmount;
-}
+let updateSlider = slideAmount => {
+  const sliderDiv = document.getElementById("sliderAmount");
+  sliderDiv.innerHTML = "£" + slideAmount;
+};
+
+let calAdminFee = slideAmount => {
+  let adminFeeResult = slideAmount * 0.05;
+  const adminFee = document.getElementById("adminFee");
+  adminFee.innerHTML = "£" + Math.round(adminFeeResult * 100) / 100;
+};
