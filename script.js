b3749c53f03092f38d770e729f3d26d02cf27b1a
AlisonYoon <politescarlett1@gmail.com>
2019-09-03 22:20:27 +0100

    display interest

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ let calAdminFee = slideAmount => {
   const adminFee = document.getElementById("adminFee");
   adminFee.innerHTML = "£" + Math.round(adminFeeResult * 100) / 100;
 };
+
+let calInterest = slideAmount => {
+  let interestResult;
+  const interestAmount = document.getElementById("interest");
+  if (slideAmount > 6400 && slideAmount <= 7200) {
+    interestResult = 500;
+  } else if (slideAmount > 7200) {
+    interestResult = 1000;
+  } else {
+    interestResult = 0;
+  }
+  interestAmount.innerHTML = "£" + interestResult;
+};
